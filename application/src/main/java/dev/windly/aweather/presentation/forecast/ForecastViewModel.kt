72beb5a091b08600c9f34fe5bf6a7ebbe059d58d
Tomasz Dzieniak <tomaszdzieniak@gmail.com>
2023-03-16 14:43:55 +0100

    Add list item scaffolds

--- application/src/main/java/dev/windly/aweather/presentation/forecast/ForecastViewModel.kt
+++ application/src/main/java/dev/windly/aweather/presentation/forecast/ForecastViewModel.kt
@@ -2,9 +2,12 @@ package dev.windly.aweather.presentation.forecast
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.windly.aweather.weather.WeatherRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class ForecastViewModel @Inject constructor(
-  private val arguments: ForecastArguments
+  private val arguments: ForecastArguments,
+  private val repository: WeatherRepository
 ) : ViewModel()
+
